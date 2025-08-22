967989fe5e7184e92202d6a9944d6ddfe0f9a2e9
Ali Ahmad <aliahmadazhar456789@gmail.com>
2025-08-22 04:04:51 -0700

    sacend e page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const Navebar =()=> {
     return (
-<div className='flex justify-between items-center p-2 bg-Whight-500 text-black-500'>
+<div className='flex justify-between items-center p-2 bg-Whight-500 text -black-500'>
     <Link href="/">Home page</Link>
     <Link href="/about">About page</Link>
-    <Link href="/contact">Contact page</Link>
-</div>
+    <Link href="/contact">Contact page</Link> 
+    </div>
     )
 }
 
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react'
 
 const page = () => {
-    return (
-        <div>ali</div>
-    )
+  return (
+    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/oip.webp" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “contact.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        AI
+      </div>
+    </figcaption>
+  </div>
+</figure></div>
+  )
 }
 
-export default page;
+export default page
